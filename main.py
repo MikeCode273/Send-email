2a797fbbdd69cd6ca4518a90c557c5274880bed6
MikeCoder <mkfenteng@st.ug.edu.gh>
2024-08-15 14:54:58 +0000

    file upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import smtplib
-my_email = "mickeyfent3@gamial.com"
+my_email = "qwamefenteng@gamial.com"
+my_password = "abcd@1234"
 
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
 
+connection.login(user=my_email,password=my_password)
 
-connection = smtplib.SMTP("smtp.gmail.cow                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ")
+connection.sendmail(from_addr=my_email, to_addrs="mickeyfent3@gmail.com", msg="Hello")
+connection.close()
