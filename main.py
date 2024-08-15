8c1c63c27d028e390c255adad05cad9da13803c2
MikeCoder <mkfenteng@st.ug.edu.gh>
2024-08-15 15:57:04 +0000

    datetime file tutorial uploaded

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import smtplib
-my_email = "qwamefenteng@gamial.com"
-my_password = "abcd@1234"
+# import smtplib
+# my_email = "qwamefenteng@gamial.com"
+# my_password = "abcd@1234"
+#
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+#
+# connection.login(user=my_email,password=my_password)
+#
+# connection.sendmail(from_addr=my_email, to_addrs="mickeyfent3@gmail.com", msg="Hello")
+# connection.close()
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
 
-connection.login(user=my_email,password=my_password)
+import datetime as dt
 
-connection.sendmail(from_addr=my_email, to_addrs="mickeyfent3@gmail.com", msg="Hello")
-connection.close()
+now = dt.datetime.now()
+
+month = now.month
+year = now.year
+hour = now.time()
+day = now.weekday()
+
+date_of_birth = dt.datetime(day= 13, month=3, year=2004, hour=12)
+
+print(date_of_birth)
